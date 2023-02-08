dbf176ab917dda27dd8616130d16dc80c1d14295
Mardson Santana <mardsonsantana105@gmail.com>
2023-02-08 19:25:26 -0300

    inicia o projeto

---
+++ src/main/java/br/com/clienteveiculos/ClienteVeiculos/ClienteVeiculosApplication.java
@@ -0,0 +1,13 @@
+package br.com.clienteveiculos.ClienteVeiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/clienteveiculos/ClienteVeiculos/ClienteVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clienteveiculos.ClienteVeiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
