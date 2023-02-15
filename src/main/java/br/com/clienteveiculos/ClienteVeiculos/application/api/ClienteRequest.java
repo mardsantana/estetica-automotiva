a5d343e67df3d0e21086550e7693cf63639a7f6f
Mardson Santana <mardsonsantana105@gmail.com>
2023-02-15 19:01:42 -0300

    implementa Response e Request

--- src/main/java/br/com/clienteveiculos/ClienteVeiculos/application/api/ClienteRequest.java
+++ src/main/java/br/com/clienteveiculos/ClienteVeiculos/application/api/ClienteRequest.java
@@ -1,4 +1,21 @@
 package br.com.clienteveiculos.ClienteVeiculos.application.api;
 
+import br.com.clienteveiculos.ClienteVeiculos.cliente.domain.Sexo;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Value
 public class ClienteRequest {
+    @NotBlank
+    private String nomeCompleto;
+    @NotBlank
+    @Email
+    private String email;
+    private String celular;
+    private String whatsapp;
+    private Sexo sexo;
+    @CPF
+    private String cpf;
 }
--- src/main/java/br/com/clienteveiculos/ClienteVeiculos/application/api/ClienteResponse.java
+++ src/main/java/br/com/clienteveiculos/ClienteVeiculos/application/api/ClienteResponse.java
@@ -1,4 +1,9 @@
 package br.com.clienteveiculos.ClienteVeiculos.application.api;
 
+import lombok.Value;
+import org.hibernate.validator.constraints.UUID;
+
+@Value
 public class ClienteResponse {
+    private UUID idCliente;
 }
