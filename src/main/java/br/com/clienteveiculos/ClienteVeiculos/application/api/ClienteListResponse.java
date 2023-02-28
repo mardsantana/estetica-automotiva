21a5b4b926555f36d7f5d4e74d7f79f1a0dfc918
Mardson Santana <mardsonsantana105@gmail.com>
2023-02-28 16:49:28 -0300

    implementa getTodos. Teste Ok!

--- src/main/java/br/com/clienteveiculos/ClienteVeiculos/application/api/ClienteListResponse.java
+++ src/main/java/br/com/clienteveiculos/ClienteVeiculos/application/api/ClienteListResponse.java
@@ -3,8 +3,10 @@ package br.com.clienteveiculos.ClienteVeiculos.application.api;
 import br.com.clienteveiculos.ClienteVeiculos.cliente.domain.Cliente;
 import lombok.Value;
 import org.hibernate.validator.constraints.UUID;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Value
 public class ClienteListResponse {
@@ -16,6 +18,16 @@ public class ClienteListResponse {
     private String whatsapp;
 
     public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-        return null;
+        return clientes.stream()
+                .map(ClienteListResponse::new)
+                .collect(Collectors.toList());
+    }
+    private ClienteListResponse(@NotNull Cliente cliente) {
+        this.idCliente = getIdCliente();
+        this.nomeCompleto = cliente.getNomeCompleto();
+        this.cpf = cliente.getCpf();
+        this.email = cliente.getEmail();
+        this.celular = cliente.getCelular();
+        this.whatsapp = cliente.getWhatsapp();
     }
 }
